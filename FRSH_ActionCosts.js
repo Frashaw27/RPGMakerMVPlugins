f9e2be51881d89a3b48ebd5631bfa75923e40be4
Frashaw27 <122448062+Frashaw27@users.noreply.github.com>
2023-03-31 06:55:35 -0400

    Update FRSH_ActionCosts.js

--- FRSH_ActionCosts.js
+++ FRSH_ActionCosts.js
@@ -1,7 +1,7 @@
 //=============================================================================
 // FRSH_ActionCosts
 // FRSH_ActionCosts.js
-// Version: 1.0.0
+// Version: 1.1.0
 //=============================================================================
 
 var Imported = Imported || {};
@@ -14,6 +14,40 @@ Frashaw.ACosts = Frashaw.ACosts || {};
 /*:
 * @author Frashaw27
 * @plugindesc Allows the maker to make skills cost extra actions
+*
+* @param displayAct
+* @text Display Needed Actions?
+* @type boolean
+* @desc Click True or False if you want to show the # of needed actions to use the skill in the skill cost.
+* @default true
+*
+* @param fontText
+* @text Action Cost Text
+* @type text
+* @desc The text used to display the number of needed actions. Use % to represent action number/All. EX: %Acts
+* @default % Acts
+*
+* @param allText
+* @text All Action Cost Text
+* @type text
+* @desc The text used to display needing all actions.
+* @default All
+*
+* @param fontSize
+* @text Action Cost Size
+* @type number
+* @min 0
+* @desc Font size used for action requirements. Use 0 to use the default font size.
+* @default 24
+*
+* @param fontColor
+* @text Action Cost Color
+* @type number
+* @min 0
+* @max 31
+* @desc The text color used for action requirements.
+* @default 0
+*
 * @help
 * ==Notetags==================================================================
 * Use all actions: <actionCost: all>
@@ -30,11 +64,26 @@ Frashaw.ACosts = Frashaw.ACosts || {};
 * the actions needed, can't overflow it to use multi action skills after
 * all other actions.
 * ===Change Log===============================================================
-* Version 1.0.0 (02/08/23):
+* Version 1.1.0 (03/31/23):
+* -Added Actions Required Display
+*
+* Version 1.0.0 (03/30/23):
 * -Finished Base Plugin
 * ============================================================================
 */
 //============================================================================
+//Sets up the information got from the plugin parameters
+Parameters = PluginManager.parameters('FRSH_ActionCosts');
+Frashaw.Param = Frashaw.Param || {};
+if (Parameters.displayAct === "true"){
+	Frashaw.Param.displayAct = true;
+} else {
+	Frashaw.Param.displayAct = false;
+}
+Frashaw.Param.fontText = Parameters.fontText;
+Frashaw.Param.allText = Parameters.allText;
+Frashaw.Param.fontSize = Number(Parameters.fontSize);
+Frashaw.Param.fontColor = Number(Parameters.fontColor);
 
 (function() {
 	//Overwrite of the make actions function
@@ -212,6 +261,60 @@ Frashaw.ACosts = Frashaw.ACosts || {};
 		} 
 		var bool = this._tp >= this.skillTpCost(skill) && this._mp >= this.skillMpCost(skill);
 		return bool && bool2 && bool3;
+	};
+	
+	//Tacks on the action cost display
+	_frsh_draw_actions = Window_SkillList.prototype.drawSkillCost;
+	Window_SkillList.prototype.drawSkillCost = function(skill, x, y, width) {
+		//Checks to see if the actions cost is in there
+		if ($dataSkills[skill.id].meta.actionCost != null){
+			//Draws the display
+			width = this.drawActions(skill, x, y, width);
+		}
+		//Does the rest of the cost displays
+		return _frsh_draw_actions.call(this,skill, x, y, width);
+	};
+	
+	//Draws the actions proper
+	Window_SkillList.prototype.drawActions = function(skill, wx, wy, dw) {
+		//returns if action cost doesn't exist
+		if ($dataSkills[skill.id].meta.actionCost == null) return dw;
+		//checks if the font size is going to be use the system default or not
+		if (Frashaw.Param.fontSize != 0){
+			//sets the text size to the parameter
+			this.contents.fontSize = Frashaw.Param.fontSize	
+		} else { 
+			//sets the text size to the system normal
+			this.contents.fontSize = this.standardFontSize();
+		}
+		//changes the text color to the parameter
+		this.changeTextColor(this.textColor(Frashaw.Param.fontColor));
+		//checks if the proper text should use an all or a number attach
+		if ($dataSkills[skill.id].meta.actionCost.toLowerCase().includes('all')){
+			//attaches the all text
+			var text = Frashaw.Param.allText + "";
+		} else {
+			//attaches the number text
+			var text = Number($dataSkills[skill.id].meta.actionCost) + "";
+		}
+		//gets the proper name of the actions display
+		var useText = Frashaw.Param.fontText.split("%");
+		//adds the text
+		text += useText[1];
+		//draws the text proper
+		this.drawText(text, wx, wy, dw, 'right');
+		//checks if skill core is imported
+		if (Imported.YEP_SkillCore) {
+			//uses yanfly skill core's cost padding
+			var returnWidth = dw - this.textWidth(text) - Yanfly.Param.SCCCostPadding;
+		} else {
+			//uses normal cost padding
+			var returnWidth = dw - this.textWidth(text) - this.textPadding();
+		}
+		this.resetTextColor();
+		this.resetFontSettings();
+		//returns the width for proper texr shifting
+		return returnWidth;
 	};
 
 })();
