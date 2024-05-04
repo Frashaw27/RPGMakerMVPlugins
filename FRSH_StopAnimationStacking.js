9f7d0471cbe58f6e357313978815d8318760b8b7
Frashaw27 <122448062+Frashaw27@users.noreply.github.com>
2024-05-04 01:49:59 -0400

    Update FRSH_StopAnimationStacking.js

--- FRSH_StopAnimationStacking.js
+++ FRSH_StopAnimationStacking.js
@@ -1,7 +1,7 @@
 //=============================================================================
 // FRSH_StopAnimationStacking
 // FRSH_StopAnimationStacking.js
-// Version: 1.0.0
+// Version: 1.1.0
 //=============================================================================
 
 var Imported = Imported || {};
@@ -16,6 +16,11 @@ Frashaw.SAStack = Frashaw.SAStack || {};
 * @plugindesc Stops animations from repeating, creating visual chaos.
 *
 * @help
+* ===Plugin Commands==========================================================
+* ! - Case Sensitive
+* animationStacking enable - Overrides the plugin and intentionally allows
+* animation stacking.
+* animationStacking enable - Turns off said override.
 * ===Introduction=============================================================
 * RPG Maker sometimes causes animations to stack up and look off and bad,
 * this plugin attempts to fix that.
@@ -23,12 +28,16 @@ Frashaw.SAStack = Frashaw.SAStack || {};
 * Just add it and it will work. Will only work in Battles as to not interupt
 * the flow of events in game that specifically call them.
 * ===Change Log===============================================================
+* Version 1.1.0 (05/04/24):
+* -Added plugin commands that allow overriding the plugins basic function
+*
 * Version 1.0.0 (04/07/24):
 * -Finished Base Plugin
 * ============================================================================
 */
 //============================================================================
 (function() {
+var override = false;
 frsh_sastack_play = Sprite_Base.prototype.startAnimation;
 Sprite_Base.prototype.startAnimation = function(animation, mirror, delay) {
 	var check = true;
@@ -37,7 +46,21 @@ Sprite_Base.prototype.startAnimation = function(animation, mirror, delay) {
 		this._animationSprites.forEach(function(i){ list.push(i._animation.id) });
 		if (list.contains(animation.id)) check = false;
 	}
+	if (override) check = true;
     if (check) frsh_sastack_play.call(this, animation, mirror, delay);
+};
+
+frsh_sastack_disable = Game_Interpreter.prototype.pluginCommand
+Game_Interpreter.prototype.pluginCommand = function(command, args) {
+	frsh_sastack_disable.call(this,command, args);
+	if (command == "animationStacking"){
+		if (args == "enable"){
+			override = true;
+		} else if (args == "disable"){
+			override = false;
+		}
+	}
+	return true;
 };
 })();
 //=============================================================================
